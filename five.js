a151a15afe4d742de5a010d180205df395c9cd9c
Mounika <mounikagurram366@gmail.com>
2023-11-24 15:10:06 +0530

    test

---
+++ five.js
@@ -0,0 +1,22 @@
+let person = { 
+    firstName: "John", 
+    lastName: "Doe", 
+    phone: 12345 
+} 
+console.log(delete person.phone); 
+console.log(person);
+
+
+var emp=[1,5,7,3]
+console.log(emp)
+for(var i=0;i<=emp.length-1;i++){
+    console.log(emp[i])
+}
+for(var emps of emp){
+     console.log(emps)
+}
+
+for (counter=1; counter<=200; counter++) {
+ document.write(counter + "  ")
+}
+ 
---
+++ four.js
@@ -0,0 +1,5 @@
+var a=[1,3,5,3,5]
+var a=[1,3,6,8,5]
+console.log(a)
+var d=2**3
+console.log(d)
